f1bd00b4f33028d3ff6b52de241ff2fe10a727ed
Lyna YUZUHA <85403524+lilyharuna@users.noreply.github.com>
2025-03-06 23:48:57 +0900

    Update autorename.rb

--- Formular/autorename.rb
+++ Formular/autorename.rb
@@ -1,1 +1,15 @@
+class Autorename < Formula
+  desc "Auto rename files in a watched directory based on given options"
+  homepage "https://github.com/lilyharuna/autorename"
+  url "https://github.com/lilyharuna/autorename/archive/v1.0.0.tar.gz"
+  sha256 "PLACEHOLDER_SHA256"  # 上で計算した sha256 に置き換える
+  version "1.0.0"
 
+  def install
+    bin.install "bin/autorename"
+  end
+
+  test do
+    system "#{bin}/autorename", "--help"
+  end
+end
