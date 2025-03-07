1697fc40a01e87148bef7f511c6f71f40c7c6b6a
Lyna YUZUHA <85403524+lilyharuna@users.noreply.github.com>
2025-03-08 04:35:49 +0900

    Version 1.0.2 released

--- Formula/autorename.rb
+++ Formula/autorename.rb
@@ -1,9 +1,9 @@
 class Autorename < Formula
   desc "Auto rename files in a watched directory based on given options"
   homepage "https://github.com/lilyharuna/autorename"
-  url "https://github.com/lilyharuna/autorename/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "d8053ff38a7207dc8865e3e2c38624d74288eced2b3062e1d0d170eab3f9b3cf"
-  version "1.0.1"
+  url "https://github.com/lilyharuna/autorename/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "89f6ae25408e95ee45027bbca9edfc27f03a82317a70142e61312584ed62dbe5"
+  version "1.0.2"
   license "BSD-3-Clause"
 
   def install
