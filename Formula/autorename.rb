7a9fa9f90a7bf7b84c1b7a7edfb16380458a9fe5
Lyna YUZUHA <85403524+lilyharuna@users.noreply.github.com>
2025-03-08 04:11:53 +0900

    Update autorename.rb

--- Formula/autorename.rb
+++ Formula/autorename.rb
@@ -3,7 +3,7 @@ class Autorename < Formula
   homepage "https://github.com/lilyharuna/autorename"
   url "https://github.com/lilyharuna/autorename/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "d8053ff38a7207dc8865e3e2c38624d74288eced2b3062e1d0d170eab3f9b3cf"
-  version "1.0.0"
+  version "1.0.1"
   license "BSD-3-Clause"
 
   def install
