dd8b0a86a5401776a620e22b9b99faa1dd7c06b4
Lyna YUZUHA <85403524+lilyharuna@users.noreply.github.com>
2025-05-21 13:07:27 +0900

    depends on fswatch

--- Formula/autorename.rb
+++ Formula/autorename.rb
@@ -5,9 +5,17 @@ class Autorename < Formula
   sha256 "256944b71fd54daa501bbeb496deb59eea60079630e8fa4838472158bb6f7aea"
   version "1.0.3"
   license "BSD-3-Clause"
+  depends_on "fswatch"
 
   def install
     bin.install "bin/autorename"
+  end
+
+  def caveats
+    <<~EOS
+      autorename uses fswatch for directory monitoring.
+      If you use this script outside Homebrew, make sure fswatch is installed and available in your PATH.
+    EOS
   end
 
   test do
